c33cebe503e0f08553a879e39f8806adec957119
Dmitrii Belousov <belousovdmn@gmail.com>
2019-08-27 18:04:49 +0000

    Added index page

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 from flask_migrate import Migrate
 
 def create_app():
@@ -10,12 +10,19 @@ def create_app():
 
     app.config.from_pyfile('config.py', silent = True)
 
-    from models import db
+    from .models import db, Ticket
 
     migrate = Migrate(app, db)
 
+    db.init_app(app)
+
     @app.route('/')
-    def hello_world():
-        return 'Hello, world!'
+    def get_index():
+       return redirect(url_for('index'))
+
+    @app.route('/tickets', methods=('GET', 'POST'))
+    def index():
+        tickets = db.session.query(Ticket).all()
+        return render_template('ticket_index.html', tickets=tickets)
 
     return app
